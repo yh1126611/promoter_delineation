1d06b81e19251495501a85ae772bcdf557d72a2f
Young Ho Lee <64393740+yh1126611@users.noreply.github.com>
2026-03-20 15:26:57 +0900

    Update delineation_calculator.py

--- delineation_calculator.py
+++ delineation_calculator.py
@@ -1,7 +1,7 @@
 # There must be a file in the directory which has columns: 1. Distance and 2. Median MP, which should be entered as input argument.
 # Base on median MP of each distance from TSS, this script will calculate where whole promoter starts & ends.
 # Core promoter size can be inputted.
-# Usage: python3 speed_promoter_delineations_calculator_keep_arg.py <Median_file> <Core_size (Opt'l, 0 if not set)>
+# Usage: delineation_calculator_keep_arg.py <Median_file> <Core_size (Opt'l, 0 if not set)>
 
 import pandas as pd
 import numpy as np
